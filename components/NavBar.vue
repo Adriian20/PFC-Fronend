a1fc9560a633b8d5f191c007275064671aca2bff
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-07 21:45:36 +0200

    Corregido estilo boton menu de usuario

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,25 +5,42 @@
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white">
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-green-800 hover:text-white focus:outline-none focus:ring-1 focus:ring-inset focus:ring-white"
+          >
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Abrir Menu</span>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
-        <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
           <div class="hidden sm:ml-6 sm:block">
             <div class="flex space-x-4">
-              <a v-for="item in navigation" :key="item.name" :href="item.href"
-                :class="[item.current ? 'bg-green-900 text-white' : 'text-gray-50 hover:bg-green-800 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium']"
-                :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-green-900 text-white'
+                    : 'text-gray-50 hover:bg-green-800 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
             </div>
           </div>
         </div>
-        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-          <button type="button"
-            class="relative rounded-full bg-green-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-green-800">
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <button
+            type="button"
+            class="relative rounded-full bg-green-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-green-800"
+          >
             <span class="absolute -inset-1.5" />
             <span class="sr-only">Carrito de compras</span>
             <ShoppingCartIcon class="h-6 w-6" aria-hidden="true" />
@@ -33,32 +50,64 @@
           <Menu as="div" class="relative ml-3">
             <div>
               <MenuButton
-                class="relative flex rounded-full bg-green-800 text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-green-800">
+                class="relative rounded-full bg-green-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 focus:ring-offset-green-800"
+              >
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Abri menu de usuario</span>
                 <UserIcon class="h-6 w-6" aria-hidden="true" />
               </MenuButton>
             </div>
-            <transition enter-active-class="transition ease-out duration-100"
-              enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100"
-              leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100"
-              leave-to-class="transform opacity-0 scale-95">
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
               <MenuItems
-                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
                 <MenuItem v-slot="{ active }" v-if="!isLogged()">
-                <a href="/login" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Iniciar
-                  Sesión</a>
+                  <a
+                    href="/login"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Iniciar Sesión</a
+                  >
                 </MenuItem>
                 <MenuItem v-slot="{ active }" v-if="!isLogged()">
-                <a href="/register"
-                  :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Registrarse</a>
+                  <a
+                    href="/register"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Registrarse</a
+                  >
                 </MenuItem>
                 <MenuItem v-slot="{ active }" v-if="isLogged()">
-                <a href="/profile" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Tú
-                  Perfil</a>
+                  <a
+                    href="/profile"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Tú Perfil</a
+                  >
                 </MenuItem>
                 <MenuItem v-slot="{ active }" v-if="isLogged()">
-                <a href="/" @click="logout()" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Cerrar Sesión</a>
+                  <a
+                    href="/"
+                    @click="logout()"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Cerrar Sesión</a
+                  >
                 </MenuItem>
               </MenuItems>
             </transition>
@@ -69,9 +118,18 @@
 
     <DisclosurePanel class="sm:hidden">
       <div class="space-y-1 px-2 pb-3 pt-2">
-        <DisclosureButton v-for="item in navigation" :key="item.name" :href="item.href"
-          :class="[item.current ? 'bg-green-900 text-white' : 'hover:bg-green-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']"
-          :aria-current="item.current ? 'page' : undefined">{{ item.name }}
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-green-900 text-white'
+              : 'hover:bg-green-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}
         </DisclosureButton>
       </div>
     </DisclosurePanel>
@@ -79,30 +137,45 @@
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon, ShoppingCartIcon, UserIcon } from '@heroicons/vue/24/outline'
-import axios from 'axios';
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import {
+  Bars3Icon,
+  BellIcon,
+  XMarkIcon,
+  ShoppingCartIcon,
+  UserIcon,
+} from "@heroicons/vue/24/outline";
+import axios from "axios";
 
 const navigation = [
-  { name: 'Página de inicio', href: '/', current: true },
-  { name: 'Categorías', href: '/categories', current: false },
-  { name: 'Artículos', href: '/articles', current: false },
-  { name: 'Visitas', href: '/visits', current: false },
-]
+  { name: "Página de inicio", href: "/", current: true },
+  { name: "Categorías", href: "/categories", current: false },
+  { name: "Artículos", href: "/articles", current: false },
+  { name: "Visitas", href: "/visits", current: false },
+];
 
-const isLogged = () => localStorage.getItem('token') !== null;
+const isLogged = () => localStorage.getItem("token") !== null;
 
 const logout = async () => {
   try {
     const token = {
-      token: localStorage.getItem('token')
+      token: localStorage.getItem("token"),
     };
-    const response = await axios.post('http://localhost:8080/pfc/users/logoutUser', token);
-    localStorage.removeItem('token');
-
+    const response = await axios.post(
+      "http://localhost:8080/pfc/users/logoutUser",
+      token
+    );
+    localStorage.removeItem("token");
   } catch (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
   }
 };
-
 </script>
