79b1d745605645b2e60b7d6d616e551ceb6d1ee5
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-15 18:00:29 +0200

    Footer creado y añadido

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <HomePage />
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import HomePage from '~/components/HomePage.vue';
+import Footer from '~/components/Footer.vue'
 </script>
 
 <style></style>
