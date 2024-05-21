dd4b8191aac8ad7e36c54e74c2cc6163699c89e0
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-21 21:25:14 +0200

    Pagina de error creada

--- error.vue
+++ error.vue
@@ -1,16 +1,11 @@
 <template>
   <div
-    class="flex flex-col items-center justify-center min-h-screen bg-pattern text-white"
+    class="flex flex-col items-center justify-center min-h-screen bg-pattern text-white h-screen"
   >
     <div class="max-w-md text-center">
-      <img
-        src="/football-icon.svg"
-        alt="Icono Futbol"
-        class="w-24 h-24 mx-auto mb-6"
-      />
       <h1 class="text-4xl font-bold text-gray-900 mb-2">¡Oops!</h1>
       <p class="text-lg text-gray-300 mb-6">
-        {{ error.message || "La página que buscas no se pudo encontrar." }}
+        {{"La página que buscas no se pudo encontrar." }}
       </p>
       <div>
         <NuxtLink to="/" class="text-blue-300 hover:underline mr-4"
