de562d8f910974a959fa73acf95fc8df02912534
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-22 19:59:05 +0200

    Footer movido a default

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <HomePage />
-    <Footer />
   </div>
 </template>
 
 <script setup>
 import HomePage from '~/components/HomePage.vue';
-import Footer from '~/components/Footer.vue';
 </script>
 
 <style></style>
