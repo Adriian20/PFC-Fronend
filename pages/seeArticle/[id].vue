52aae9d9f258b72c45d951a99004060f5863094f
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-25 19:25:28 +0200

    Añadir al carrito

--- pages/seeArticle/[id].vue
+++ pages/seeArticle/[id].vue
@@ -44,6 +44,7 @@
         </p>
         <div class="mb-4 mt-12 flex w-full items-center gap-3 md:w-1/2">
           <button
+            @click="addToCart"
             class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none w-52"
             type="button"
             data-ripple-light="true"
@@ -58,15 +59,19 @@
 
 <script setup>
 import axios from "axios";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import { useCartStore } from "@/stores/cart";
+import { useToast } from "vue-toastification";
+
 const route = useRoute();
+const articulo = ref([]);
+const cartStore = useCartStore();
+const toast = useToast();
+const isLogged = ref(false);
+const loaded = ref(false);
 
-let articulo = ref([]);
-
-const getImageUrl = (imageName) => {
-  return `/images/${imageName}`;
-};
+const getImageUrl = (imageName) => `/images/${imageName}`;
 
 async function showArticles() {
   try {
@@ -74,6 +79,8 @@ async function showArticles() {
       "http://localhost:8080/pfc/articles/article/" + route.params.id
     );
     articulo.value = response.data;
+    isLogged.value = checkIsLogged();
+    loaded.value = true;
   } catch (error) {
     console.error("Error fetching articles:", error);
     return {
@@ -85,7 +92,30 @@ async function showArticles() {
   }
 }
 
-showArticles();
+function checkIsLogged() {
+  return (
+    typeof localStorage !== "undefined" &&
+    localStorage.getItem("token") !== null
+  );
+}
+
+const addToCart = () => {
+  cartStore.addItem({
+    id: articulo.value.id,
+    nombre: articulo.value.nombre,
+    precio: articulo.value.precio,
+    img: articulo.value.img,
+    descripcion: articulo.value.descripcion,
+    talla: articulo.value.talla,
+    marca: articulo.value.marca,
+  });
+
+  toast.success("Artículo añadido al carrito");
+};
+
+onMounted(() => {
+  showArticles();
+});
 </script>
 
 <style></style>
