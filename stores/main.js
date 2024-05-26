6ae74b9272551f54940412bef2bff53d640c3a67
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-26 12:35:17 +0200

    Que al recargar no se vacie el carrito

--- stores/main.js
+++ stores/main.js
@@ -1,11 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import { useCartStore } from '@/stores/cart';
 
-const app = createApp(App)
-const pinia = createPinia()
+const app = createApp(App);
+const pinia = createPinia();
 
-app.use(pinia)
-app.use(router)
-app.mount('#app')
+app.use(pinia);
+
+const cartStore = useCartStore();
+cartStore.initializeCart();
+
+app.mount('#app');
