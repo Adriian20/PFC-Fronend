1d3863ca8468c2d06fc71afa69f8fbdd2e3d9351
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-31 20:16:35 +0200

    Importacion simplificada

--- plugins/vue-toastification.js
+++ plugins/vue-toastification.js
@@ -3,22 +3,5 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast, {
-    transition: "Vue-Toastification__bounce",
-    maxToasts: 3,
-    newestOnTop: true,
-    position: "top-right",
-    timeout: 2000,
-    closeOnClick: true,
-    pauseOnFocusLoss: false,
-    pauseOnHover: false,
-    draggable: true,
-    draggablePercent: 0.6,
-    showCloseButtonOnHover: false,
-    hideProgressBar: true,
-    closeButton: "button",
-    icon: true,
-    rtl: false,
-    timeoutProgressBar: true,
-  });
+  nuxtApp.vueApp.use(Toast);
 });
