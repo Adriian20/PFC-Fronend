8a5c37dd34524a68be26c4f5e64c8f6c9f757eef
Adriian20 <adrianmarquesroqueta@gmail.com>
2024-05-31 20:19:52 +0200

    Toast configurado

--- plugins/vue-toastification.js
+++ plugins/vue-toastification.js
@@ -3,5 +3,22 @@ import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast);
+  nuxtApp.vueApp.use(Toast, {
+    transition: "Vue-Toastification__bounce",
+    maxToasts: 3,
+    newestOnTop: true,
+    position: "top-right",
+    timeout: 2000,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+    timeoutProgressBar: true,
+  });
 });
