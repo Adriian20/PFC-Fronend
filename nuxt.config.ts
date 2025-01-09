c689075a50b8540e4737986fae8d86148de8f19c
Adriian20 <adrianmarquesroqueta@gmail.com>
2025-01-09 20:54:16 +0100

    Added app head information

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,34 @@ export default defineNuxtConfig({
     };
   })(),
 
+  app: {
+    head: {
+      title: "FUTBOLMANÍA VINTAGE",
+      htmlAttrs: {
+        lang: "es",
+      },
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: "TiendaFutbol",
+        },
+      ],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/jugador-de-futbol.png",
+        },
+      ],
+    },
+  },
+
   css: [
     "~/assets/css/tailwind.css",
     "vuetify/lib/styles/main.sass",
